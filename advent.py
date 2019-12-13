8a53f96b238bc868e3390bd3722f39f2f605b3c4
Peter Zalewski <peter@zalewski.com>
2019-12-12 21:17:32 -0500

    Complete day 1, part 2

--- advent.py
+++ advent.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 def calculate_fuel_requirement(mass):
-  assert(mass > 0)
-  return (mass // 3) - 2
+  if mass <= 0:
+    return 0
+
+  fuel_mass = max(0, (mass // 3) - 2)
+  return fuel_mass + calculate_fuel_requirement(fuel_mass)
 
 def day1():
-  with open('day1/input.txt', 'r') as module_weights:
-    return sum(calculate_fuel_requirement(int(mass)) for mass in module_weights)
+  with open('day1/input.txt', 'r') as module_masses:
+    return sum(calculate_fuel_requirement(int(mass)) for mass in module_masses)
 
 if __name__ == '__main__':
   print(day1())
